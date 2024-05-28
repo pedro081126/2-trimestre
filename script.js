06e05c40b708d60cb131588d5de5829ad8855e76
pedro081126 <128717103+pedro081126@users.noreply.github.com>
2024-05-28 08:46:28 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+const caixaPrincipal = document.querySelector(".caixa-pricipal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
+const perguntas = [
+    {
+        enunciado:"Você gosta de bolo de chocolate?", 
+        alternativas:[
+            "Sim",
+            "Não",
+        ]
+    },
+    {
+        enunciado:"E de bolo de cenoura?", 
+        alternativas:[
+            "Sim",
+            "Não",
+        ]
+    },
+    {
+        enunciado:"Gosta de bolo de milho?", 
+        alternativas:[
+            "Sim",
+            "Não",
+        ]
+    },
+    {
+        enunciado:"Bolo de fubá tbm?", 
+        alternativas:[
+            "Sim",
+            "Não",
+        ]
+    },
+    {
+        enunciado:"Vocẽ gosta mesmo de bolo?", 
+        alternativas:[
+            "Sim",
+            "Não",
+        ]
+    },
+];
